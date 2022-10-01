2c04fedfa78245855486b858a73e2d02b7e5eed5
rifatrakib <Rakib.1508@outlook.com>
2022-10-01 08:24:37 +0600

    pre-commit hooks added and installed

--- server/auth/controller.py
+++ server/auth/controller.py
@@ -1,3 +1,1 @@
-# This script contains the business logic (e.g., database query and manipulation,
-# data processing, data update and delete, etc.) used in the authentication
-# and authorization endpoints.
+# controller.py
