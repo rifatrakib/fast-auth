c69ea7c9aaf4efaa1ae02bb9f66899407bc33db8
rifatrakib <Rakib.1508@outlook.com>
2022-10-03 10:34:51 +0600

    added schema for password with validator

--- server/auth/schemas.py
+++ server/auth/schemas.py
@@ -1,3 +1,4 @@
+import json
 import re
 from datetime import datetime
 from enum import Enum
@@ -24,7 +25,8 @@ class UserBase(BaseModel):
     @validator("phone_number")
     def phone_number_validation(cls, value):
         regex = r"^(\+)[1-9][0-9\-\(\)\.]{9,15}$"
-        if value and not re.search(regex, value, re.I):
+        pat = re.compile(regex)
+        if value and not re.search(pat, value, re.I):
             raise ValueError("phone number invalid")
 
         return value
@@ -36,6 +38,30 @@ class UserCreate(UserBase):
 
 class User(UserBase):
     is_active: bool
+
+
+class Password(BaseModel):
+    password: str
+
+    @validator("password")
+    def password_validator(cls, value):
+        regex = r"""
+            ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])([A-Za-z\d@$!%*#?&]{6,32}$
+        """
+        pat = re.compile(regex)
+        invalid_message = {
+            "message": "password must have the following fields",
+            "uppercase letters": 1,
+            "lowercase letters": 1,
+            "special characters (@$!%*#?&)": 1,
+            "length": {"min": 6, "max": 32},
+        }
+        if not value:
+            raise ValueError("no password provided")
+        elif not re.search(pat, value):
+            raise ValueError(json.dumps(invalid_message))
+
+        return value
 
 
 class AuthToken(BaseModel):
