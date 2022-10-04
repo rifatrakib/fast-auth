b747f0792bf027da2038982f57eb3fad85ffd793
rifatrakib <Rakib.1508@outlook.com>
2022-10-04 11:44:01 +0600

    setup async sqlalchemy session with databases

--- server/database.py
+++ server/database.py
@@ -1,11 +1,11 @@
+import databases
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 from .utils import get_url
 
 SQLALCHEMY_DATABASE_URL = get_url()
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = databases.Database(SQLALCHEMY_DATABASE_URL)
 Base = declarative_base()
