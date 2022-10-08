d46b6b152d17021891fbd9f9b99ad21d58f4b932
rifatrakib <Rakib.1508@outlook.com>
2022-10-08 13:01:42 +0600

    add new crud functions to get user

--- server/auth/crud.py
+++ server/auth/crud.py
@@ -14,8 +14,22 @@ async def create_new_user(session: AsyncSession, user: schemas.UserCreate):
     return user_data
 
 
-async def get_user(session: AsyncSession, user_id: int):
+async def get_user_by_id(session: AsyncSession, user_id: int):
     query = select(models.User).filter(models.User.id == user_id)
     user_data = await session.execute(query)
     user = user_data.scalars().first()
+    return user
+
+
+async def get_user_by_username(session: AsyncSession, username: str):
+    query = select(models.User).filter(models.User.username == username)
+    user_data = await session.execute(query)
+    user = user_data.scalars().first().__dict__
+    return user
+
+
+async def get_user_by_email(session: AsyncSession, email_address: str):
+    query = select(models.User).filter(models.User.email_address == email_address)
+    user_data = await session.execute(query)
+    user = user_data.scalars().first().__dict__
     return user
