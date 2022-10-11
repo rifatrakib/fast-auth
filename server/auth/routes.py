f96c1e4636228e1e98c6069c2028bf2b5b44f0c0
rifatrakib <Rakib.1508@outlook.com>
2022-10-11 08:31:48 +0600

    validate password with password schema validator method on register

--- server/auth/routes.py
+++ server/auth/routes.py
@@ -8,23 +8,19 @@ from ..config import read_config
 from ..database import get_session
 from .crud import create_new_user, get_user_by_id
 from .dependencies import get_current_user
-from .schemas import AuthToken, User, UserCreate
+from .schemas import AuthToken, Password, User, UserCreate, UserInRequest
 from .utils import authenticate_user, create_access_token, get_password_hash
 
 router = APIRouter(prefix="/auth")
 
 
-@router.post("/users/", response_model=User)
-async def create_user(user: UserCreate, session: AsyncSession = Depends(get_session)):
-    user.hashed_password = get_password_hash(user.hashed_password)
-    user = await create_new_user(session, user)
-    return user
-
-
-@router.get("/users/", response_model=User, dependencies=[Depends(get_current_user)])
-async def read_user(user_id: int, session: AsyncSession = Depends(get_session)):
-    user = await get_user_by_id(session, user_id)
-    return user
+@router.post("/register/", response_model=User)
+async def register_new_user(user: UserInRequest, session: AsyncSession = Depends(get_session)):
+    validated_password = Password(password=user.password)
+    hashed_password = get_password_hash(validated_password.password)
+    user_data = UserCreate(**user.dict(), hashed_password=hashed_password)
+    created_user = await create_new_user(session, user_data)
+    return created_user
 
 
 @router.post("/login/", response_model=AuthToken)
@@ -41,3 +37,9 @@ async def login_for_access_token(
     access_token_expires = timedelta(minutes=read_config("access_token_expire_minutes"))
     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/users/", response_model=User, dependencies=[Depends(get_current_user)])
+async def read_user(user_id: int, session: AsyncSession = Depends(get_session)):
+    user = await get_user_by_id(session, user_id)
+    return user
