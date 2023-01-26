98ddbe3585494d368fc86b3243be499d19f27188
rifatrakib <Rakib.1508@outlook.com>
2023-01-26 14:43:04 +0600

    use mode from env to fetch different settings

--- server/core/config.py
+++ server/core/config.py
@@ -1,6 +1,11 @@
 from functools import lru_cache
 
+from decouple import config
+
 from server.core.environments.base import BaseConfig
+from server.core.environments.development import DevelopmentConfig
+from server.core.environments.production import ProductionConfig
+from server.core.environments.staging import StagingConfig
 
 
 class SettingsFactory:
@@ -8,13 +13,17 @@ class SettingsFactory:
         self.mode = mode
 
     def __call__(self) -> BaseConfig:
-        if self.mode == "development":
-            return BaseConfig()
+        if self.mode == "staging":
+            return StagingConfig()
+        elif self.mode == "production":
+            return ProductionConfig()
+        else:
+            return DevelopmentConfig()
 
 
 @lru_cache()
 def get_settings() -> SettingsFactory:
-    return SettingsFactory(mode="development")()
+    return SettingsFactory(mode=config("MODE", default="development", cast=str))()
 
 
 settings: SettingsFactory = get_settings()
