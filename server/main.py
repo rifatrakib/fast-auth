b52558c26aeabcbb528e58f05611f3d9ceb2aa5e
rifatrakib <Rakib.1508@outlook.com>
2023-01-29 12:58:03 +0600

    create template for authentication endpoints

--- server/main.py
+++ server/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
 from server.core.config import settings
+from server.routers.auth import router as auth_router
 from server.routers.user import router as user_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(auth_router)
 
 
 @app.get("/health")
