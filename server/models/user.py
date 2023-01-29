8f74432b5debb6553cdfd78b02145460667f6eb2
rifatrakib <Rakib.1508@outlook.com>
2023-01-29 16:03:33 +0600

    add and update user and account models to store basic info

--- server/models/user.py
+++ server/models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.schema import FetchedValue
 from sqlalchemy.sql import functions
 
@@ -14,9 +15,9 @@ class Account(Base):
     hashed_password = Column(String(1024), nullable=True)
     hash_salt = Column(String(1024), nullable=True)
     phone_number = Column(String(16), unique=True, index=True)
-    is_active = Column(Boolean, nullable=False, default=False)
-    is_verified = Column(Boolean, nullable=False, default=False)
-    is_logged_in = Column(Boolean, nullable=False, default=False)
+    is_active = Column(Boolean, nullable=False, default=False, index=True)
+    is_verified = Column(Boolean, nullable=False, default=False, index=True)
+    is_logged_in = Column(Boolean, nullable=False, default=False, index=True)
     created_at = Column(
         DateTime(timezone=True),
         nullable=False,
@@ -27,3 +28,25 @@ class Account(Base):
         nullable=True,
         server_onupdate=FetchedValue(for_update=True),
     )
+
+    user = relationship("User", back_populates="account", uselist=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(
+        Integer,
+        ForeignKey("accounts.id"),
+        ondelete="CASCADE",
+        index=True,
+        unique=True,
+    )
+    first_name = Column(String(64), nullable=False)
+    middle_name = Column(String(256), default=None)
+    last_name = Column(String(64), nullable=False)
+    gender = Column(String(1), default=None)
+    birthday = Column(DateTime(timezone=True), default=None)
+
+    account = relationship("Account", back_populates="user")
