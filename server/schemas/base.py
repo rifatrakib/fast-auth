3301b721ebe44694aad99d053f2f627e15c94f78
rifatrakib <Rakib.1508@outlook.com>
2023-02-04 07:27:43 +0600

    add new base schema for non-db facing models

--- server/schemas/base.py
+++ server/schemas/base.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Extra
 
 from server.services.formatters import (
     format_datetime_into_isoformat,
@@ -12,7 +12,14 @@ from server.services.formatters import (
 class BaseSchemaORM(BaseModel):
     class Config:
         orm_mode: bool = True
+        use_enum_values: bool = True
         validate_assignment: bool = True
         allow_population_by_field_name: bool = True
         json_encoders: dict = {datetime: format_datetime_into_isoformat}
+
+
+class BaseSchemaAPI(BaseModel):
+    class Config:
+        extra = Extra.forbid
+        allow_population_by_field_name: bool = True
         alias_generator: Any = format_dict_key_to_camel_case
