323c4e36492192ec8efca9c6d05c5c885c577004
rifatrakib <Rakib.1508@outlook.com>
2023-02-10 02:10:26 +0600

    complete user signup process

--- server/routers/auth.py
+++ server/routers/auth.py
@@ -1,8 +1,46 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from server.schemas.account import SignupRequestSchema, SignupResponseSchema
+from server.schemas.token import AccountToken
+from server.security.dependencies import generate_crud_instance
+from server.security.token import jwt_generator
+from server.services.exceptions import EntityAlreadyExists
+from server.services.messages import http_exc_400_credentials_bad_signup_request
+from server.sql.user import AccountCRUD
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
-@router.get("/signup")
-async def register_user():
-    return {"message": "trial"}
+@router.post(
+    "/signup",
+    name="auth:signup",
+    summary="Create new user",
+    response_model=SignupResponseSchema,
+)
+async def register_user(
+    payload: SignupRequestSchema,
+    account: AccountCRUD = Depends(generate_crud_instance(name=AccountCRUD)),
+):
+    try:
+        await account.is_username_available(username=payload.username)
+        await account.is_email_available(email=payload.email)
+    except EntityAlreadyExists:
+        raise await http_exc_400_credentials_bad_signup_request()
+
+    new_user = await account.create_account(data=payload)
+    access_token = jwt_generator.generate_access_token(account=new_user)
+
+    return SignupResponseSchema(
+        id=new_user.id,
+        authentication_token=AccountToken(
+            token=access_token,
+            username=new_user.username,
+            email=new_user.email,
+            phone_number=new_user.phone_number,
+            is_verified=new_user.is_verified,
+            is_active=new_user.is_active,
+            is_logged_in=new_user.is_logged_in,
+            created_at=new_user.created_at,
+            updated_at=new_user.updated_at,
+        ),
+    )
