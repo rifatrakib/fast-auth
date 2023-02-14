a1df58c9beb60047205ae844c023d6dad52f418f
rifatrakib <Rakib.1508@outlook.com>
2023-02-14 08:03:08 +0600

    added dependency function for decoding token

--- server/security/dependencies.py
+++ server/security/dependencies.py
@@ -1,10 +1,15 @@
 from typing import Callable, Type
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from server.database import get_session
+from server.schemas.token import JWTData
+from server.security.token import jwt_generator
 from server.sql.base import SQLBase
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/signin")
 
 
 def generate_crud_instance(name: Type[SQLBase]) -> Callable[[], SQLBase]:
@@ -14,3 +19,17 @@ def generate_crud_instance(name: Type[SQLBase]) -> Callable[[], SQLBase]:
         return name(session=session)
 
     return _create_crud_instance
+
+
+async def decode_user_token(
+    token: str = Depends(oauth2_scheme),
+):
+    try:
+        user_data: JWTData = jwt_generator.retrieve_token_details(token)
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail={"msg": "could not validate credentials"},
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user_data
