a4f3ec13492a8f9efc21139f13d2bcb62f3adaa4
rifatrakib <Rakib.1508@outlook.com>
2023-02-14 08:05:32 +0600

    added higher level dependency to restrict based on status

--- server/security/dependencies.py
+++ server/security/dependencies.py
@@ -5,6 +5,7 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from server.database import get_session
+from server.models.user import Account
 from server.schemas.token import JWTData
 from server.security.token import jwt_generator
 from server.services.exceptions import EntityDoesNotExist
@@ -48,5 +49,27 @@ async def get_current_user(
             status_code=status.HTTP_403_FORBIDDEN,
             detail={"msg": "could not validate credentials"},
             headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    user: Account = Depends(get_current_user),
+):
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"msg": "inactive user"},
+        )
+    return user
+
+
+async def get_current_verified_user(
+    user: Account = Depends(get_current_user),
+):
+    if not user.is_verified:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"msg": "inactive user"},
         )
     return user
