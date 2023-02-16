79f01a804e3f10657a6051df3cb4115e6f13a8df
rifatrakib <Rakib.1508@outlook.com>
2023-02-16 07:41:25 +0600

    use http status code handling functions for denying access

--- server/security/dependencies.py
+++ server/security/dependencies.py
@@ -1,6 +1,6 @@
 from typing import Callable, Type
 
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -9,10 +9,15 @@ from server.models.user import Account
 from server.schemas.token import JWTData
 from server.security.token import jwt_generator
 from server.services.exceptions import EntityDoesNotExist
+from server.services.messages import (
+    http_exc_400_inactive_user,
+    http_exc_400_unverified_user,
+    http_exc_403_credentials_exception,
+)
 from server.sql.base import SQLBase
 from server.sql.user import AccountCRUD
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/signin")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/signin")
 
 
 def generate_crud_instance(name: Type[SQLBase]) -> Callable[[], SQLBase]:
@@ -30,11 +35,7 @@ async def decode_user_token(
     try:
         user_data: JWTData = jwt_generator.retrieve_token_details(token)
     except ValueError:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail={"msg": "could not validate credentials"},
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        raise await http_exc_403_credentials_exception()
     return user_data
 
 
@@ -45,11 +46,7 @@ async def get_current_user(
     try:
         user = await account.read_account_by_username(user_data.username)
     except EntityDoesNotExist:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail={"msg": "could not validate credentials"},
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        raise await http_exc_403_credentials_exception()
     return user
 
 
@@ -57,10 +54,7 @@ async def get_current_active_user(
     user: Account = Depends(get_current_user),
 ):
     if not user.is_active:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail={"msg": "inactive user"},
-        )
+        raise await http_exc_400_inactive_user()
     return user
 
 
@@ -68,8 +62,5 @@ async def get_current_verified_user(
     user: Account = Depends(get_current_user),
 ):
     if not user.is_verified:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail={"msg": "inactive user"},
-        )
+        raise await http_exc_400_unverified_user()
     return user
