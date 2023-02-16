55ebc4bbc9b309ca11fbfd531b61dc442bfd1058
rifatrakib <Rakib.1508@outlook.com>
2023-02-16 07:39:22 +0600

    added new functions for handling different http status codes

--- server/services/messages.py
+++ server/services/messages.py
@@ -4,5 +4,41 @@ from fastapi import HTTPException, status
 async def http_exc_400_credentials_bad_signup_request() -> Exception:
     return HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
-        detail="Signup failed! Recheck all your credentials!",
+        detail={"msg": "Signup failed! Recheck all your credentials!"},
+    )
+
+
+async def http_exc_400_credentials_bad_signin_request() -> Exception:
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail={"msg": "Signin failed! Recheck all your credentials!"},
+    )
+
+
+async def http_exc_400_inactive_user() -> Exception:
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail={"msg": "User not active. Please reactive account!"},
+    )
+
+
+async def http_exc_400_unverified_user() -> Exception:
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail={"msg": "User not verified. Please verify your phone number!"},
+    )
+
+
+async def http_exc_403_credentials_exception() -> Exception:
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail={"msg": "Refused access to the requested resource!"},
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+
+async def http_exc_403_forbidden_request() -> Exception:
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail={"msg": "Refused access to the requested resource!"},
     )
