7e974fcbf46826f4dc7d7577d0c91856c71b9e1b
rifatrakib <Rakib.1508@outlook.com>
2023-02-17 07:37:46 +0600

    add new variables for mailing configuration

--- server/core/environments/base.py
+++ server/core/environments/base.py
@@ -1,4 +1,6 @@
-from pydantic import BaseSettings
+from typing import Union
+
+from pydantic import BaseSettings, EmailStr
 
 
 class BaseConfig(BaseSettings):
@@ -25,6 +27,17 @@ class BaseConfig(BaseSettings):
     JWT_MIN: int
     JWT_HOUR: int
     JWT_DAY: int
+
+    # mail server config
+    MAIL_USERNAME: Union[EmailStr, str]
+    MAIL_PASSWORD: str
+    MAIL_FROM: EmailStr
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+    MAIL_STARTTLS: bool
+    MAIL_SSL_TLS: bool
+    USE_CREDENTIALS: bool
 
     class Config:
         env_file = ".env"
