8b11ff634de8202570ee2667d990de14a3967560
rifatrakib <Rakib.1508@outlook.com>
2023-02-18 19:08:19 +0600

    schema for data in jwt updated with necessary fields

--- server/schemas/token.py
+++ server/schemas/token.py
@@ -6,22 +6,11 @@ from pydantic import EmailStr
 from server.schemas.base import BaseSchemaAPI
 
 
-class AccountToken(BaseSchemaAPI):
-    token: str
-    username: str
-    email: EmailStr
-    phone_number: Union[str, None] = None
-    is_verified: bool
-    is_active: bool
-    is_logged_in: bool
-    created_at: datetime
-    updated_at: Union[datetime, None] = None
-
-
 class JWTData(BaseSchemaAPI):
     id: int
     username: str
-    email: str
+    email: EmailStr
+    phone_number: Union[str, None] = None
 
 
 class JWToken(BaseSchemaAPI):
