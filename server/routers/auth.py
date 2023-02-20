f8c02632c7cdb3410c4a0282c1da877f787df722
rifatrakib <Rakib.1508@outlook.com>
2023-02-20 07:33:37 +0600

    add endpoint for activation clicked from email

--- server/routers/auth.py
+++ server/routers/auth.py
@@ -20,6 +20,7 @@ from server.services.messages import (
     http_exc_400_credentials_bad_signin_request,
     http_exc_400_credentials_bad_signup_request,
     http_exc_400_inactive_user,
+    http_exc_404_key_expired,
 )
 from server.sql.user import AccountCRUD, AccountValidationCRUD
 
@@ -76,3 +77,23 @@ async def signin(
     access_token = jwt_generator.generate_access_token(account=user)
 
     return AuthResponseSchema(token_type="Bearer", access_token=access_token)
+
+
+@router.get(
+    "/activate-account/{validation_key}",
+    name="auth:activation",
+    summary="Activate user account with random validation key",
+    response_model=MessageResponseSchema,
+    status_code=status.HTTP_202_ACCEPTED,
+)
+async def activate_account(
+    validation_key: str,
+    account: AccountCRUD = Depends(generate_crud_instance(name=AccountCRUD)),
+    validator: AccountValidationCRUD = Depends(generate_crud_instance(name=AccountValidationCRUD)),
+):
+    try:
+        account_to_activate = await validator.delete_account_validation(validation_key=validation_key)
+        await account.activate_account(account_to_activate.account_id)
+    except EntityDoesNotExist:
+        raise await http_exc_404_key_expired()
+    return MessageResponseSchema(msg="Your account has been activated")
