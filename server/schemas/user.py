2d2649ce8ca1756952ce7dbd22e20bc5895e5cf7
rifatrakib <Rakib.1508@outlook.com>
2023-02-24 12:37:51 +0600

    added schema to receive password update request

---
+++ server/schemas/user.py
@@ -0,0 +1,24 @@
+from pydantic import Field
+
+from server.schemas.base import BaseSchemaAPI
+
+
+class UpdateUserBase(BaseSchemaAPI):
+    id: int = Field(
+        title="user ID",
+        decription="Unique ID that can be used to distinguish between users.",
+    )
+
+
+class UpdateUserPassword(UpdateUserBase):
+    password: str = Field(
+        title="password",
+        decription="""
+            Password containing at least 1 uppercase letter, 1 lowercase letter,
+            1 number, 1 character that is neither letter nor number, and
+            between 8 to 32 characters.
+        """,
+        regex=r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,64}$",
+        min_length=8,
+        max_length=64,
+    )
