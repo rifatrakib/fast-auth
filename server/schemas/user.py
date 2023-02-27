1e4109e2b00ab13654480ea9fbf4e2370f4dc87c
rifatrakib <Rakib.1508@outlook.com>
2023-02-27 07:29:32 +0600

    add new response schema for user info

--- server/schemas/user.py
+++ server/schemas/user.py
@@ -1,6 +1,33 @@
+from datetime import datetime
+from typing import Union
+
 from pydantic import Field
 
-from server.schemas.base import BaseSchemaAPI
+from server.schemas.base import BaseSchemaAPI, BaseSchemaORM
+from server.schemas.token import JWTData
+
+
+class UserInformationResponse(JWTData, BaseSchemaORM):
+    is_active: bool = Field(
+        default=False,
+        title="active status",
+        decription="Whether user is active, determined via email confirmation.",
+    )
+    is_verified: bool = Field(
+        default=False,
+        title="verification status",
+        decription="Whether user is verified, determined via phone number verification.",
+    )
+    created_at: datetime = Field(
+        default_factory=datetime.utcnow,
+        title="time of insert",
+        decription="Exact time when the user information was first saved.",
+    )
+    updated_at: Union[datetime, None] = Field(
+        default=None,
+        title="time of last update",
+        decription="Exact time when the user information was last updated.",
+    )
 
 
 class UpdateUserPassword(BaseSchemaAPI):
