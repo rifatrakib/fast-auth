c4517b41c6446444063bc771dfe8b4967b9f9eeb
rifatrakib <Rakib.1508@outlook.com>
2023-03-15 14:38:19 +0600

    add endpoints to get user information

--- server/routers/user.py
+++ server/routers/user.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Union
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Path, status
 
 from server.models.user import Account
 from server.schemas.user import UserResponseSchema
@@ -14,8 +14,8 @@ from server.security.dependencies import (
     last_name_form_field,
     middle_name_form_field,
 )
-from server.services.exceptions import EntityAlreadyExists
-from server.services.messages import http_exc_409_conflict
+from server.services.exceptions import EntityAlreadyExists, EntityDoesNotExist
+from server.services.messages import http_exc_404_not_found, http_exc_409_conflict
 from server.services.validators import Tags
 from server.sql.user import UserCRUD
 
@@ -56,3 +56,45 @@ async def create_new_user(
         "gender": new_user.gender,
         "birthday": new_user.birthday,
     }
+
+
+@router.get(
+    "/me",
+    name="user:read-user",
+    summary="Read a user",
+    response_model=UserResponseSchema,
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(get_current_active_user)],
+)
+async def read_this_user(
+    user: UserCRUD = Depends(generate_crud_instance(name=UserCRUD)),
+    current_user: Account = Depends(get_current_active_user),
+):
+    try:
+        print(f"{current_user.id = }")
+        user = await user.read_user_by_account_id(current_user.id)
+        print(f"{user = }")
+        return user
+    except EntityDoesNotExist:
+        raise await http_exc_404_not_found()
+
+
+@router.get(
+    "/{user_id}",
+    name="user:read-user",
+    summary="Read a user",
+    response_model=UserResponseSchema,
+    status_code=status.HTTP_200_OK,
+)
+async def read_user(
+    user_id: int = Path(
+        title="user ID",
+        decription="Unique ID that can be used to distinguish between users.",
+    ),
+    user: UserCRUD = Depends(generate_crud_instance(name=UserCRUD)),
+):
+    try:
+        user = await user.read_user_by_id(user_id)
+        return user
+    except EntityDoesNotExist:
+        raise await http_exc_404_not_found()
